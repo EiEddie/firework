54b897fbc34ab246573688e7bc748037a085f5fb
EiEddie <eieddie4u@outlook.com>
2024-02-03 17:05:53 +0800

    fix: 分拆不同类型火箭

--- src/arg.rs
+++ src/arg.rs
@@ -1,27 +1,29 @@
-/// 大火箭的质量
-pub(crate) const BIG_ROCKET_MASS: f64 = 50.;
+use std::ops::{Range, RangeInclusive};
 
-/// 小火箭的质量
-pub(crate) const SMALL_ROCKET_MASS: f64 = 1.;
+impl crate::BigRocket {
+	/// 颜色范围: RGB
+	pub(crate) const COLOR_RANGE: (RangeInclusive<u8>, RangeInclusive<u8>, RangeInclusive<u8>) =
+		(0..=255, 0..=255, 0..=255);
+	/// 质量
+	pub(crate) const MASS: f64 = 50.;
+	/// 水平初速度的范围
+	pub(crate) const SPEED_RANGE_X: Range<f64> = -5.0..5.0;
+	/// 竖直初速度的范围
+	pub(crate) const SPEED_RANGE_Y: Range<f64> = 20.0..55.0;
+	/// 轨迹扩散的范围
+	pub(crate) const TRAIL_SPREAD: f64 = 0.5;
+}
 
-/// 大火箭初速度的范围
-pub(crate) const BIG_ROCKET_SPEED_Y: [f64; 2] = [20., 55.];
-pub(crate) const BIG_ROCKET_SPEED_X: f64 = 5.;
-
-/// 小火箭初速度的范围
-pub(crate) const SMALL_ROCKET_SPEED: [f64; 2] = [5., 150.];
-
-/// 大火箭轨迹扩散的范围
-pub(crate) const BIG_ROCKET_TRAIL_SPREAD: f64 = 0.5;
-
-/// 小火箭轨迹扩散的范围
-pub(crate) const SAMLL_ROCKET_TRAIL_SPREAD: f64 = 1.;
-
-/// 大火箭颜色范围: RGB
-pub(crate) const BIG_ROCKET_COLOR_RANGE: [(u8, u8, u8); 2] = [(50, 100, 70), (70, 100, 100)];
-
-/// 小火箭寿命
-pub(crate) const SMALL_ROCKET_AGE: f64 = 3.;
+impl crate::SmallRocket {
+	/// 寿命
+	pub(crate) const AGE: f64 = 3.;
+	/// 质量
+	pub(crate) const MASS: f64 = 1.;
+	/// 初速度的范围
+	pub(crate) const SPEED_RANGE: Range<f64> = 5.0..150.0;
+	/// 轨迹扩散的范围
+	pub(crate) const TRAIL_SPREAD: f64 = 1.;
+}
 
 pub(crate) trait CanvasSize {
 	fn width(&self) -> u32;
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use colorsys::{Hsl, Rgb};
 use rand::{self, Rng};
 mod arg;
-use arg::*;
 
 #[derive(Debug)]
 struct Vec2<T>(T, T);
@@ -10,7 +9,24 @@ type Vec2f = Vec2<f64>;
 type Vec2i = Vec2<i32>;
 
 #[derive(Debug)]
-struct Rocket {
+struct BigRocket {
+	/// 位置
+	pos: Vec2f,
+	/// 质量
+	mas: f64,
+	/// 速度
+	vel: Vec2f,
+
+	/// 颜色
+	color: Hsl,
+
+	/// 扩散范围
+	/// 可以制作模糊的轨迹
+	spread: f64,
+}
+
+#[derive(Debug)]
+struct SmallRocket {
 	/// 位置
 	pos: Vec2f,
 	/// 质量
@@ -25,31 +41,27 @@ struct Rocket {
 	/// 可以制作模糊的轨迹
 	spread: f64,
 
-	is_small: bool,
-	/// 如果是火箭的小颗粒
-	/// 则它存在寿命
-	age:      f64,
+	/// 寿命
+	age: f64,
 }
 
-impl Rocket {
-	fn new_big(size: &impl arg::CanvasSize) -> Self {
+impl BigRocket {
+	fn launch(size: &impl arg::CanvasSize) -> Self {
 		let mut rng = rand::thread_rng();
 
 		let p_x = rng.gen_range(0.0..size.width() as f64);
 
-		let v_x = rng.gen_range(-BIG_ROCKET_SPEED_X..BIG_ROCKET_SPEED_X);
-		let v_y = -rng.gen_range(BIG_ROCKET_SPEED_Y[0]..BIG_ROCKET_SPEED_Y[1]);
+		let v_x = rng.gen_range(Self::SPEED_RANGE_X);
+		let v_y = -rng.gen_range(Self::SPEED_RANGE_Y);
 
-		let c_r = rng.gen_range(BIG_ROCKET_COLOR_RANGE[0].0..BIG_ROCKET_COLOR_RANGE[1].0);
-		let c_g = rng.gen_range(BIG_ROCKET_COLOR_RANGE[0].1..BIG_ROCKET_COLOR_RANGE[1].1);
-		let c_b = rng.gen_range(BIG_ROCKET_COLOR_RANGE[0].2..BIG_ROCKET_COLOR_RANGE[1].2);
+		let c_r = rng.gen_range(Self::COLOR_RANGE.0);
+		let c_g = rng.gen_range(Self::COLOR_RANGE.1);
+		let c_b = rng.gen_range(Self::COLOR_RANGE.2);
 
-		Self { pos:      Vec2(p_x, 0.),
-		       mas:      BIG_ROCKET_MASS,
-		       vel:      Vec2(v_x, v_y),
-		       color:    Rgb::from((c_r, c_g, c_b)).into(),
-		       spread:   BIG_ROCKET_TRAIL_SPREAD,
-		       is_small: false,
-		       age:      f64::MAX, }
+		Self { pos:    Vec2(p_x, 0.),
+		       mas:    Self::MASS,
+		       vel:    Vec2(v_x, v_y),
+		       color:  Rgb::from((c_r, c_g, c_b)).into(),
+		       spread: Self::TRAIL_SPREAD, }
 	}
 }
